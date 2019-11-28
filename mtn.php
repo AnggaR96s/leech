b05ea327415636dd287cdb076be9a190d173af7a
ARSLҽʋαɳƚҽιɳ <kiritoclasher@gmail.com>
2019-11-28 20:10:51 +0700

    mtn: config fix derp on last commit

--- mtn.php
+++ mtn.php
@@ -57,7 +57,7 @@ foreach($files as $file)
                                                          <option value=2> 2 </option>
                                                          <option value=3> 3 </option>
                                                          <option value=4> 4 </option>
-                                                         <option value=5> 5 </option></select> x <select name=rs><option value= > 1 </option>
+                                                         <option value=5 selected> 5 </option></select> x <select name=rs><option value= > 1 </option>
                                                                                                                 <option value=2> 2 </option>
                                                                                                                 <option value=3> 3 </option>
                                                                                                                 <option value=4> 4 </option>
